e521a0a6bfe8f53022b9efb7005b6ce9b587487e
Klaus Ma <klausm@nvidia.com>
2025-12-28 04:46:58 +0000

    fix: spawn a thread for cache.
    Signed-off-by: Klaus Ma <klausm@nvidia.com>

--- executor_manager/src/main.rs
+++ executor_manager/src/main.rs
@@ -11,6 +11,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+use std::thread;
+
 use clap::Parser;
 use futures::future::join_all;
 use tokio::runtime::{Builder, Runtime};
@@ -58,7 +60,25 @@ async fn main() -> Result<(), FlameError> {
     // The manager thread will start one thread for each executor.
     let max_executors = ctx.executors.limits.max_executors as usize;
     let manager_rt = build_runtime("manager", max_executors + 1)?;
-    let cache_rt = build_runtime("cache", max_executors + 1)?;
+    let cache_rt = build_runtime("cache", 3)?;
+
+    {
+        let ctx = ctx.clone();
+        thread::spawn(move || {
+            let _ = cache_rt.spawn(async move {
+                // Start the object cache server.
+                if let Some(cache_config) = ctx.cache {
+                    cache::run(&cache_config).await
+                } else {
+                    tracing::warn!(
+                        "No object cache configuration found, skipping object cache thread."
+                    );
+                    Ok(())
+                }
+            });
+            // handlers.push(handler);
+        });
+    }
 
     // Start executor manager thread.
     {
@@ -67,22 +87,6 @@ async fn main() -> Result<(), FlameError> {
             // Create the executor manager by the context.
             let mut manager = ExecutorManager::new(&ctx).await?;
             manager.run().await
-        });
-        handlers.push(handler);
-    }
-
-    {
-        let ctx = ctx.clone();
-        let handler = cache_rt.spawn(async move {
-            // Start the object cache server.
-            if let Some(cache_config) = ctx.cache {
-                cache::run(&cache_config).await
-            } else {
-                tracing::warn!(
-                    "No object cache configuration found, skipping object cache thread."
-                );
-                Ok(())
-            }
         });
         handlers.push(handler);
     }
